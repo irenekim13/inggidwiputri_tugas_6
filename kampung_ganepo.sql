a7fa4c52a1d7abc04db5d11a934a9370d7a6cc65
irenekim13 <inggidwiputri0gmail.com>
2019-03-15 21:35:57 +0700

    sasa

---
+++ kampung_ganepo.sql
@@ -0,0 +1,71 @@
+-- phpMyAdmin SQL Dump
+-- version 4.8.3
+-- https://www.phpmyadmin.net/
+--
+-- Host: 127.0.0.1
+-- Generation Time: Mar 15, 2019 at 03:34 PM
+-- Server version: 10.1.37-MariaDB
+-- PHP Version: 7.2.12
+
+SET SQL_MODE = "NO_AUTO_VALUE_ON_ZERO";
+SET AUTOCOMMIT = 0;
+START TRANSACTION;
+SET time_zone = "+00:00";
+
+
+/*!40101 SET @OLD_CHARACTER_SET_CLIENT=@@CHARACTER_SET_CLIENT */;
+/*!40101 SET @OLD_CHARACTER_SET_RESULTS=@@CHARACTER_SET_RESULTS */;
+/*!40101 SET @OLD_COLLATION_CONNECTION=@@COLLATION_CONNECTION */;
+/*!40101 SET NAMES utf8mb4 */;
+
+--
+-- Database: `kampung_ganepo`
+--
+
+-- --------------------------------------------------------
+
+--
+-- Table structure for table `data_anggota`
+--
+
+CREATE TABLE `data_anggota` (
+  `id` int(11) NOT NULL,
+  `nama` varchar(64) NOT NULL,
+  `no_hp` varchar(16) NOT NULL,
+  `jenis_kelamin` varchar(16) NOT NULL,
+  `alamat` varchar(255) NOT NULL,
+  `keanggotaan` varchar(16) NOT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=latin1;
+
+--
+-- Dumping data for table `data_anggota`
+--
+
+INSERT INTO `data_anggota` (`id`, `nama`, `no_hp`, `jenis_kelamin`, `alamat`, `keanggotaan`) VALUES
+(1, 'zainal azis', '082172211222', 'laki-laki', 'padang', '1Ganepo 99'),
+(4, 'inggi dwi putri', '0984437554564', 'laki-laki', 'padang', 'Ganepo 99');
+
+--
+-- Indexes for dumped tables
+--
+
+--
+-- Indexes for table `data_anggota`
+--
+ALTER TABLE `data_anggota`
+  ADD PRIMARY KEY (`id`);
+
+--
+-- AUTO_INCREMENT for dumped tables
+--
+
+--
+-- AUTO_INCREMENT for table `data_anggota`
+--
+ALTER TABLE `data_anggota`
+  MODIFY `id` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=5;
+COMMIT;
+
+/*!40101 SET CHARACTER_SET_CLIENT=@OLD_CHARACTER_SET_CLIENT */;
+/*!40101 SET CHARACTER_SET_RESULTS=@OLD_CHARACTER_SET_RESULTS */;
+/*!40101 SET COLLATION_CONNECTION=@OLD_COLLATION_CONNECTION */;
